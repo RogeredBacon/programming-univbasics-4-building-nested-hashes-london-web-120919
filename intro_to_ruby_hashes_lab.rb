4d260d84e128a0aba1d021a8ec1f893177ee69d4
James Inglis <jinglis12@googlemail.com>
2019-11-29 12:26:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-	  railroads: {pieces: 4, rent_in_dollars: {}},
+	  railroads: {pieces: 4, rent_in_dollars: {one_piece_owned: 24, two_pieces_owned: 1, three_pieces_owned: 1, four_pieces_owned: 1}},
 	}
 end
 
