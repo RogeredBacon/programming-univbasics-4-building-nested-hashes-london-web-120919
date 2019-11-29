f8c1a44657183e0937fc44234efd9d815a7f3414
James Inglis <jinglis12@googlemail.com>
2019-11-29 12:30:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,6 +12,16 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
+	  railroads: {pieces: 4},
+	}
+end
+
+def monopoly_with_third_tier
+  # When you start writing the implementation for this method, copy the Hash
+  # you built in the previous method. Run the tests. They will fail, but
+  # they'll guide you in how to modify what you just did to have more
+  # complexity.
+	{
 	  railroads: {
 	    pieces: 4,
 	    rent_in_dollars: {
@@ -21,16 +31,6 @@ def monopoly_with_second_tier
 	      four_pieces_owned: 200
 	    }
 	  }
-	}
-end
-
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  {
-	  railroads: {pieces: 4},
 	}
 end
 
